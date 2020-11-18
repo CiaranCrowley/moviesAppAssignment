96ac00584379ff2631b1b84064cf9883c885c168
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-11-18 11:14:38 +0000

    Initial commit with all base files from labs up to Lab-Movie_App-Part4

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom" ;
+import FavoriteMoviesPage from './pages/favoritesMoviesPage' ;
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import HomePage from "./pages/homePage";
+import MoviePage from './pages/movieDetailsPage';
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <div className="jumbotron">
+          <SiteHeader />      
+          <div className="container-fluid">
+          <MoviesContextProvider>
+            <GenresContextProvider>
+                  <Switch>
+                    <Route exact path="/reviews/form" component={AddMovieReviewPage} />
+                    <Route path="/reviews/:id" component={MovieReviewPage} />
+                    <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+                    <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+                    <Route path="/movies/:id" component={MoviePage} />
+                    <Route path="/" component={HomePage} />
+                    <Redirect from="*" to="/" />
+                  </Switch>
+              </GenresContextProvider>
+            </MoviesContextProvider>     
+          </div>
+         </div>
+      </BrowserRouter>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
