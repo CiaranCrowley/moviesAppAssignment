815c760b1b080cdb68b65b0ecc899de8af65702f
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-11-26 14:32:07 +0000

    Fixed TypeError: Cannot read property 'map' of undefined in tvList/index.js

--- src/components/tvList/index.js
+++ src/components/tvList/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import Tv from "../tvCard/";
 import "./tvList.css";
 
-const TvList = ({tv, action}) => {
-  const tvCards = tv.map(t => (
+const TvList = ({tvShows, action}) => {
+  const tvCards = tvShows.map(t => (
     <Tv key={t.id} tv={t} action={action} />
   ));
   return <div className="row movies bg-info">{tvCards}</div>;
