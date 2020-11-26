2bd607c49b97f0234921de79b109bf9e89393e7c
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-11-26 17:56:07 +0000

    got counter working on TV Shows page

--- src/components/tvHeaderList/index.js
+++ src/components/tvHeaderList/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const TvHeader = ({ name, numShows }) => {
+const TvHeader = ({ title, numTvShows }) => {
   return (
     <div className="row">
       <div className="col-md-6 offset-4">
         <h2>
-          {`${name}  `}
-          <span className="badge badge-pill badge-success">{numShows}</span>
+          {`${title}  `}
+          <span className="badge badge-pill badge-success">{numTvShows}</span>
         </h2>
       </div>
     </div>
