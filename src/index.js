4f07faed0bdcc609e4ff52c0d73a365aecc3708b
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-11-28 16:26:50 +0000

    made changes to try and fix pipeline

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom" ;
-import FavoriteMoviesPage from './pages/favoritesMoviesPage' ;
+import FavoriteMoviesPage from "./pages/favoritesMoviesPage" ;
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
-import MoviePage from './pages/movieDetailsPage';
+import MoviePage from "./pages/movieDetailsPage";
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader';
+import SiteHeader from "./components/siteHeader";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import MoviesContextProvider from "./contexts/moviesContext";
 import GenresContextProvider from "./contexts/genresContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage';
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
 import WatchListPage from "./pages/watchListPage";
 import PopularMoviesPage from "./pages/popularMovies";
 import PopularMoviesContextProvider from "./contexts/popularMoviesContext";
 import TvShowsPage from "./pages/tvShowsPage";
 import TvShowsContextProvider from "./contexts/tvContext";
-import TvPage from './pages/tvDetailsPage';
+import TvPage from "./pages/tvDetailsPage";
 import TvReviewPage from "./pages/tvReviewPage";
 import TvAiringTodayPage from "./pages/tvAiringTodayPage";
 import FavoriteTvShowsPage from "./pages/favoriteTvShowsPage";
-import AddTvShowReviewPage from './pages/addTvShowReviewPage';
+import AddTvShowReviewPage from "./pages/addTvShowReviewPage";
 
 
 const App = () => {
