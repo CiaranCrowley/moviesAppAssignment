d90b251933dc7cf1d000982edf10e479c7fbbedb
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-11 11:39:20 +0000

    Added Drop down menus for Movies and Tv Shows to de-clutter the navbar.  Testing must be changed for this

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -1,107 +1,119 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../globals/fontawesome";
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./siteHeader.css";
 
-// const SiteHeader = () => {
-//   return (
-//     <nav className="navbar  navbar-light fixed-top  bg-dark ">
-//       <nav className="navbar-brand text-white">
-//         <Link className=" text-white" to="/">
-//           TMDB Client
-//         </Link>
-//       </nav>
-//       {/* <FontAwesomeIcon
-//         className="navbar-text text-light"
-//         icon={["fas", "video"]}
-//         size="3x"
-//       />
-//       <FontAwesomeIcon
-//         className="navbar-text text-light"
-//         icon={["fas", "film"]}
-//         size="3x"
-//       /> */}
-//       <nav className="navbar navbar-expand ">
-//         <ul className="navbar-nav">
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/">
-//               Home
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/movies/upcoming">
-//               Upcoming
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/movies/popular">
-//               Popular
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/movies/latest">
-//               Latest
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/movies/favorites">
-//               Favorites
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/movies/watchList">
-//               Watch List
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/tv">
-//               TV Shows
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/tv/top_rated">
-//               Top Rated Shows
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/tv/favoriteShows">
-//               Favorite TV Shows
-//             </Link>
-//           </li>
-//           <li className="nav-item">
-//             <Link className="nav-link text-white" to="/tv/tvWatchlist">
-//               Tv Watchlist
-//             </Link>
-//           </li>
-//         </ul>
-//       </nav>
-//     </nav>
-//   );
-// };
-
-
 const SiteHeader = () => {
-
-  
   return (
-    <html>
-      <head>
-      </head>
-      <body>
-        <div class="dropdown">
-          <button class="dropbtn">Dropdown</button>
-          <div class="dropdown-content">
-            <a href="#">Link 1</a>
-            <a href="#">Link 2</a>
-            <a href="#">Link 3</a>
+    <nav className="navbar  navbar-light fixed-top  bg-dark ">
+      <nav className="navbar-brand text-white">
+        <Link className=" text-white" to="/">
+          TMDB Client
+        </Link>
+      </nav>
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "video"]}
+        size="3x"
+      />
+      <FontAwesomeIcon
+        className="navbar-text text-light"
+        icon={["fas", "film"]}
+        size="3x"
+      />
+      <nav className="navbar navbar-expand ">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link text-white padding-right: 10px;" to="/">
+              Home
+            </Link>
+          </li>
+          <div class="dropdown">
+            <button class="dropbtn">Movies</button>
+            <div class="dropdown-content">
+            <Link className="nav-link text-white" to="/movies/upcoming">
+              Upcoming Movies
+            </Link>
+            <Link className="nav-link text-white" to="/movies/latest">
+              Latest Movies
+            </Link>
+            <Link className="nav-link text-white" to="/movies/favorites">
+              Your Favourite Movies
+            </Link>
+            <Link className="nav-link text-white" to="/movies/watchList">
+              Your Watch List
+            </Link>
           </div>
         </div>
-      </body>
-    </html>
+
+        <div class="dropdown">
+            <button class="dropbtn">Tv</button>
+            <div class="dropdown-content">
+            <Link className="nav-link text-white" to="/tv">
+              TV Shows
+            </Link>
+            <Link className="nav-link text-white" to="/tv/top_rated">
+              Top Rated Shows
+            </Link>
+            <Link className="nav-link text-white" to="/tv/favoriteShows">
+              Your Favorite Shows
+            </Link>
+            <Link className="nav-link text-white" to="/tv/tvWatchlist">
+              Your Tv Watchlist
+            </Link>
+          </div>
+        </div>
+
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/upcoming">
+              Upcoming
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/popular">
+              Popular
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/latest">
+              Latest
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/favorites">
+              Favorites
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/movies/watchList">
+              Watch List
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/tv">
+              TV Shows
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/tv/top_rated">
+              Top Rated Shows
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/tv/favoriteShows">
+              Favorite TV Shows
+            </Link>
+          </li> */}
+          {/* <li className="nav-item">
+            <Link className="nav-link text-white" to="/tv/tvWatchlist">
+              Tv Watchlist
+            </Link>
+          </li> */}
+        </ul>
+      </nav>
+    </nav>
   );
 };
-
-
 
 export default SiteHeader;
