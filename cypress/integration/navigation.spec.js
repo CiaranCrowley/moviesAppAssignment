0391cec5bcfa29f954728c534b5b11936a8a5c36
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-12 13:33:48 +0000

    Added  testing for the TV dropdown navigation

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -160,68 +160,60 @@ describe("Navigation", () => {
   //  ********************    END OF MOVIE TESTS ********************
 
   //  ********************    TV  NAVIGATION  TESTS    ********************
-  // describe("From the Tv Shows page", () => {
-  //   beforeEach(() => {
-  //     cy.visit("/");
-  //   });
-  //   it("should navigate to the show details page and change browser URL", () => {
-  //     cy.get("nav").find("li").eq(6).click();
-  //     cy.get(".card").eq(1).find("img").click();
-  //     cy.url().should("include", `/tv/${tvShows[1].id}`);
-  //     cy.get("h2").contains(tvShows[1].name);
-  //   });
-  //   it("should allow navigation from site header", () => {
-  //     cy.get("nav").find("li").eq(7).find("a").click();
-  //     cy.url().should("include", `/top_rated`);
-  //     cy.get("h2").contains("Top rated Shows");
-  //     cy.get("nav").find("li").eq(8).find("a").click();
-  //     cy.url().should("not.include", `/top_rated`);
-  //     cy.get("h2").contains("Favorite Tv Shows");
-  //     cy.get("nav").find("li").eq(7).find("a").click();
-  //     cy.get("nav.navbar-brand").find("a").click();
-  //     cy.url().should("not.include", `/favorites`);
-  //     cy.get("h2").contains("No. Movies");
-  //   });
-  // });
+  describe("From the Tv Shows page", () => {
+    it("should navigate to the show details page and change browser URL", () => {
+      cy.wait(200);
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Tv Shows").click();
+      cy.get(".card").eq(1).find("img").click();
+      cy.url().should("include", `/tv/${tvShows[1].id}`);
+      cy.get("h2").contains(tvShows[1].name);
+    });
+    it("should allow navigation from site header", () => {
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Rated").click();
+      cy.url().should("include", `/top_rated`);
+      cy.get("h2").contains("Top rated Shows");
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Your Favourite Tv Shows").click();
+      cy.url().should("not.include", `/top_rated`);
+      cy.get("h2").contains("Favorite Tv Shows");
+    });
+  });
 
-  // describe("From the Tv Show Details page ", () => {
-  //   beforeEach(() => {
-  //     cy.visit('/');
-  //     cy.get("nav").find("li").eq(6).click();
-  //     cy.get(".card").eq(0).find("img").click();
-  //   });
-  //   it("should change browser URL when show/hide reviews is clicked", () => {
-  //     cy.contains("Show Reviews").click();
-  //     cy.url().should("include", `/tv/${showId}/reviews`);
-  //     cy.contains("Hide Reviews").click();
-  //     cy.url().should("not.include", `/tv/${showId}/reviews`);
-  //   });
-  //   it("navigate to the full review page when a 'Full Review' link is clicked", () => {
-  //     cy.contains("Show Reviews").click();
-  //     cy.url().should("include", `/tv/${showId}/reviews`);
-  //     cy.contains("Full Review").click();
-  //     cy.url().should("include", `/tvReviews/${showReviewId}`);
-  //   });
-  // });
+  describe("From the Tv Show Details page ", () => {
+    beforeEach(() => {
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Tv Shows").click();
+      cy.wait(200);
+      cy.get(".card").eq(0).find("img").click();
+    });
+    it("should change browser URL when show/hide reviews is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/tv/${showId}/reviews`);
+      cy.contains("Hide Reviews").click();
+      cy.url().should("not.include", `/tv/${showId}/reviews`);
+    });
+    it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+      cy.contains("Show Reviews").click();
+      cy.url().should("include", `/tv/${showId}/reviews`);
+      cy.contains("Full Review").click();
+      cy.url().should("include", `/tvReviews/${showReviewId}`);
+    });
+  });
 
-  // describe("From Top Rated Shows Page", () => {
-  //   beforeEach(() => {
-  //     cy.visit("/");
-  //     cy.get("nav").find("li").eq(7).find("a").click();
-  //   });
-  //   //  ********************    NAVIGATES FROM TOP RATED TV SHOWS TO show details    ********************
-  //   it("should navigate to the Tv Show's detail page and change the browser URL", () => {
-  //     cy.url().should("include", `/tv/top_rated`)
-  //     cy.get(".card").eq(0).find("img").click();
-  //     cy.url().should("include", `/tv/`);
-  //   });
-  //   //  ********************    NAVIGATES FROM TOP RATED TV SHOWS TO WATCH LIST AND CLICKS TV CARD    ********************
-  //   it("should navigate to the Top Rated page, find card 0 and click the add to show watch list button ", () => {
-  //     cy.url().should("include", `/tv/top_rated`)
-  //     cy.get(".card").eq(0).find("button").click();
-  //     cy.get("nav").find("li").eq(9).find("a").click();
-  //     cy.get(".card").eq(0).find("img").click();
-  //     cy.get("h2");
-  //   });
-  // });
+  describe("From Top Rated Shows Page", () => {
+    beforeEach(() => { cy.visit("/");
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Rated").click();
+      cy.wait(200);
+    });
+    it("should navigate to the Tv Show's detail page and change the browser URL", () => {
+      cy.url().should("include", `/tv/top_rated`)
+      cy.get(".card").eq(0).find("img").click();
+      cy.url().should("include", `/tv/`);
+    });
+    it("should navigate to the Top Rated page, find card 0 and click the add to show watch list button and go to the watch list", () => {
+      cy.url().should("include", `/tv/top_rated`)
+      cy.get(".card").eq(0).find("button").click();
+      cy.get(".dropdown").contains("TV Menu").click().get('.dropdown-menu').get(".dropdown-item").contains("Watch List").click();
+      cy.get(".card").eq(0).find("img").click();
+      cy.get("h2");
+    });
+  });
 });
