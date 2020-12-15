1f22b5c5a85eeb415ca852f792fa3ab104d18602
Ciaran Crowley <ciarancrowley37@gmail.com>
2020-12-15 14:44:48 +0000

    Made new filter controls for people as it didnt make sense to have genres there

--- src/components/templatePeopleListPage/index.js
+++ src/components/templatePeopleListPage/index.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import Header from "../personHeaderList";
 import PeopleList from "../peopleList";
-import FilterControls from "../filterControls";
+import PeopleFilterControls from "../peopleFilterControls";
 
 const PeopleListPageTemplate = ({people, name, action }) => {
   const [nameFilter, setNameFilter] = useState("");
-  const [genreFilter, setGenreFilter] = useState("0");
-  const genre = Number(genreFilter)
+  // const [genreFilter, setGenreFilter] = useState("0");
+  // const genre = Number(genreFilter)
   let displayedPeople = people
     .filter(m => {
       return m.name.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
-    })
-    .filter(m => {
-      return  genre > 0
-        ? m.genre_ids.includes(Number(genreFilter))
-        : true;
     });
+    // .filter(m => {
+    //   return  genre > 0
+    //     ? m.genre_ids.includes(Number(genreFilter))
+    //     : true;
+    // });
 
   const handleChange = (type, value) => {
     if (type === "name") setNameFilter(value);
-    else setGenreFilter(value);
+    // else setGenreFilter(value);
   };
 
   return (
     <>
       <Header name={name} numPeople={displayedPeople.length} />
-      <FilterControls onUserInput={handleChange} numPeople={displayedPeople.length}/>
+      <PeopleFilterControls onUserInput={handleChange} numPeople={displayedPeople.length}/>
       <PeopleList
         action={action}
         people={displayedPeople}
